fd463108255c25a74c3650d8effa904bdbf3b78c
yorkyi <yiyusheng.hust@gmail.com>
2017-04-30 14:40:17 +0800

    0430

---
+++ load_bitstampUSD.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# Filename: load_bitstampUSD.R
+#
+# Description: 
+#
+# Copyright (c) 2017, Yusheng Yi <yiyusheng.hust@gmail.com>
+#
+# Version 1.0
+#
+# Initial created: 2017-04-27 21:51:37
+#
+# Last   modified: 2017-04-27 21:52:06
+#
+#
+#
+
+rm(list = ls());setwd('~/Code/R/COIN/');source('~/rhead')
+DT <- read.csv(file.path(dir_data,'bitstampUSD.csv'),header = F)
+names(DT) <- c('time','price','value')
+DT$time <- as.POSIXct.numeric(DT$time,tz = 'UTC',origin = '1970-01-01')
+save(DT,file = file.path(dir_data,'bitstampUSD.Rda'))
+
+
+###### ANALYSIS ######
+DT$date <- as.Date(DT$time)
+sta_date <- melt(table(DT$date))
